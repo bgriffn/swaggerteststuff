069cf17925c96a09e5a352b6b7fb6c16a3e8cd52
bgriffn <brendan.griffin@smartbear.com>
2017-04-27 15:10:57 +0000

    Generated code by SwaggerHub

---
+++ jaxrs/src/main/java/io/swagger/api/factories/PetApiServiceFactory.java
@@ -0,0 +1,13 @@
+package io.swagger.api.factories;
+
+import io.swagger.api.PetApiService;
+import io.swagger.api.impl.PetApiServiceImpl;
+
+@javax.annotation.Generated(value = "class io.swagger.codegen.languages.JavaJerseyServerCodegen", date = "2017-04-27T15:10:57.279Z")
+public class PetApiServiceFactory {
+    private final static PetApiService service = new PetApiServiceImpl();
+
+    public static PetApiService getPetApi() {
+        return service;
+    }
+}
